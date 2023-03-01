1ba90e3b31daca55c3749ec8fdfa0afd53caa7b8
Arun9181 <120468099+Arun9181@users.noreply.github.com>
2023-03-01 21:20:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App=()=>{
+    return(
+        <>
+        <div>App</div>
+        <h1> 2+2</h1>
+        <p>{5+5}</p>
+        <h3> Bengaluru</h3>
+        </>
+    )
+}
+export default App
