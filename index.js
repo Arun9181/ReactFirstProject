62a531d5ff990131bff28ccf490f93cb0a63ea41
Arun9181 <120468099+Arun9181@users.noreply.github.com>
2023-03-03 09:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {createRoot} from "react-dom/client"
+import App from './App'
+import Nav from './functionBasedComponent/Nav';
+import Logo from "./functionBasedComponent/Logo";
+import Footer from './functionBasedComponent/Footer'
+import Aside from "./functionBasedComponent/Aside";
+import Header from "./functionBasedComponent/Header";
+import Div from "./functionBasedComponent/Div";
+import Span from "./functionBasedComponent/Span";
+import Form from "./functionBasedComponent/Form";
+import Searchbar from "./functionBasedComponent/Searchbar";
+import Button from "./functionBasedComponent/Button";
+
+import Aside1 from "./classBasedComponent/Aside1";
+import Button1 from "./classBasedComponent/Button1";
+import Div1 from "./classBasedComponent/Div1";
+import Footer1 from "./classBasedComponent/Footer1";
+import Form1 from "./classBasedComponent/Form1";
+import Header1 from "./classBasedComponent/Header1";
+import Logo1 from "./classBasedComponent/Logo1";
+import Nav1 from "./classBasedComponent/Nav1";
+import Searchbar1 from "./classBasedComponent/Searchbar1";
+import Span1 from "./classBasedComponent/Span1";
+
+
+createRoot(document.getElementById('root')).render(<App/>)
+createRoot(document.getElementById('root')).render(<Nav/>)
+createRoot(document.getElementById('root')).render(<Logo/>)
+createRoot(document.getElementById('root')).render(<Footer/>)
+createRoot(document.getElementById('root')).render(<Aside/>)
+createRoot(document.getElementById('root')).render(<Header/>)
+createRoot(document.getElementById('root')).render(<Div/>)
+createRoot(document.getElementById('root')).render(<Span/>)
+createRoot(document.getElementById('root')).render(<Form/>)
+createRoot(document.getElementById('root')).render(<Searchbar/>)
+createRoot(document.getElementById('root')).render(<Button/>)
+
+createRoot(document.getElementById('root')).render(<Aside1/>)
+createRoot(document.getElementById('root')).render(<Button1/>)
+createRoot(document.getElementById('root')).render(<Div1/>)
+createRoot(document.getElementById('root')).render(<Footer1/>)
+createRoot(document.getElementById('root')).render(<Form1/>)
+createRoot(document.getElementById('root')).render(<Header1/>)
+createRoot(document.getElementById('root')).render(<Logo1/>)
+createRoot(document.getElementById('root')).render(<Nav1/>)
+createRoot(document.getElementById('root')).render(<Searchbar1/>)
+createRoot(document.getElementById('root')).render(<Span1/>)
